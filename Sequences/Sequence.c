e866d8f85c40497a6409bbb0662d6a151bd1cb9f
user name <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-19 10:04:24 +0100

    C_basics, follow my C journey by learning as I documenting C topics for begineers to understand easier

---
+++ Sequences/Sequence.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+/*
+Basic sequence in C programming
+*/
+printf("Column A\tColumn B\tColumn C");
+printf("\nMy Computer's Beep Sounds Like This\a: !\n");
+printf("\"Letz\bs fix that typo and then show the backslash");
+printf("character \\\" she said\n");
+return 0;
+}
+
+
